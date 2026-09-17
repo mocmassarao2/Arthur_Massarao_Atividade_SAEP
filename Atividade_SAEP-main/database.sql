ce11ca910522cf6560e899c73ff5fefa5fad5a34
mocmassarao2 <a.marcomini@aluno.senai.br>
2026-09-17 15:50:48 -0300

    Add files via upload

---
+++ Atividade_SAEP-main/database.sql
@@ -0,0 +1,28 @@
+-- create database sistema_eleitoral;
+-- use sistema_eleitoral
+
+/* CREATE TABLE eleitor (
+	id_eleitor INT auto_increment primary key,
+    nome varchar(100) NOT NULL,
+	numero_titulo varchar(20) NOT NULL UNIQUE,
+    cidade varchar(80)
+); */
+
+/* create table candidato (
+    id_candidato int auto_increment primary key,
+    nome varchar(100) not null,
+    numero_candidato int not null,
+    cargo varchar(50) not null
+); */
+
+/* ALTER TABLE candidato
+ADD partido_ficticio VARCHAR(50); */
+
+/* INSERT INTO eleitor (nome, numero_titulo, cidade) VALUES
+('Yasmin Kamilly', 'TIT001', 'Santo André'),
+('Sofia Nunes', 'TIT002', 'São Paulo'),
+('Isabelly Queiroz', 'TIT003', 'Santo Amaro');
+
+INSERT INTO candidato (nome, numero_candidato, cargo) VALUES
+('Candidato Alfa', 10, 'Represetante'),
+('Candidato Beta', 20, 'Represetante'); */
